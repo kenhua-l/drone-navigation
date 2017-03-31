e9fef2f2759ff7867425c5d9c448d8573f9d1913
mervynwee <mail.wyrm@gmail.com>
2017-03-31 09:42:33 +0800

    Finish writing calcDestinationCoords function as well as its dependencies. Verified working.

--- src/generate_trajectory.cpp
+++ src/generate_trajectory.cpp
@@ -5,7 +5,7 @@
 
 #include <nav_msgs/OccupancyGrid.h>
 #include <nav_msgs/Path.h>
-
+#define PI 3.14159
 class TrajectoryPlanner
 {
 	private:
@@ -28,16 +28,16 @@ class TrajectoryPlanner
 			// The next step would be to generate a *smooth* path for multiple goals
 		}
 
-		float distance(float x1, float y1, float x2, float y2)
+		double distance(double x1, double y1, double x2, double y2)
     {
       return sqrt( pow(x1 - x2, 2) + pow(y1 - y2, 2) );
     }
 
-		int find_index_nearest(nav_msgs::Path &path, float robot_x, float robot_y)
+		int find_index_nearest(nav_msgs::Path &path, double robot_x, double robot_y)
     {
       static int previous_index = 0;
 
-      float temp_distance, next_distance;
+      double temp_distance, next_distance;
       geometry_msgs::Pose temp_pose, next_pose;
       for(int i = previous_index; i < path.poses.size() - 1; i++)
       {
@@ -56,21 +56,74 @@ class TrajectoryPlanner
       return path.poses.size() - 1;
     }
 
-		void convertToLocalCoords(float robot_x, float robot_y, float robot_yaw, float x, float y, float &local_x, float &local_y)
+		void convertToLocalCoords(double robot_x, double robot_y, double robot_yaw, double x, double y, double &local_x, double &local_y)
     {
       local_x = (x - robot_x) * cos(-robot_yaw) - (y - robot_y) * sin(-robot_yaw);
       local_y = (x - robot_x) * sin(-robot_yaw) + (y - robot_y) * cos(-robot_yaw);
     }
 
 		// Find the angle between the origin and a line formed by two points
-		float findAngle(geometry_msgs::Pose pose1, geometry_msgs::Pose pose2)
+		double findAngle(geometry_msgs::Pose pose1, geometry_msgs::Pose pose2)
 		{
 			return atan2( pose2.position.y - pose1.position.y, pose2.position.x - pose1.position.x );
 		}
 
-		void rotate(float angle, float x, float, y)
+		void rotate(double angle, double x, double y, double &new_x, double &new_y)
 		{
+			new_x = x * cos(-angle) - y * sin(-angle);
+			new_y = x * sin(-angle) + y * cos(-angle);
+		}
 
+		double calcRadius(double look_ahead_local_x, double look_ahead_local_y, double robot_x, double robot_y)
+		{
+			return pow( distance(look_ahead_local_x, look_ahead_local_y, robot_x, robot_y), 2) / (2 * (look_ahead_local_x - robot_x));
+		}
+
+		void calcDestinationCoords(double robot_x, double robot_y, double robot_yaw, double look_ahead_local_x, double look_ahead_local_y, double distance_traveled, double &new_robot_x, double &new_robot_y)
+		{
+			if( (look_ahead_local_x - robot_x < 0.0001) && (look_ahead_local_x - robot_x > -0.0001) )
+			{
+				new_robot_x = robot_x + distance_traveled * cos(robot_yaw);
+				new_robot_y = robot_y + distance_traveled * sin(robot_yaw);
+			}
+			else
+			{
+				double radius = calcRadius(look_ahead_local_x, look_ahead_local_y, robot_x, robot_y);
+				// std::cout << "radius: " << radius << std::endl;
+				double theta = distance_traveled / radius;
+				// std::cout << "theta: " << theta << std::endl;
+				double temp_x, temp_y = 0;
+				if(look_ahead_local_x > 0)
+				{
+					temp_x = -radius;
+				}
+				else
+				{
+					temp_x = radius;
+				}
+				double new_x, new_y;
+				rotate(theta, temp_x, temp_y, new_x, new_y);
+				new_x = new_x - temp_x + robot_x;
+				new_y = new_y + robot_y;
+				rotate(robot_yaw + PI/2, new_x, new_y, new_x, new_y);
+				new_robot_x = new_x;
+				new_robot_y = new_y;
+			}
+
+			/* Test case for this function
+			TrajectoryPlanner tp;
+			double x, y;
+			tp.calcDestinationCoords(0, 0, 0, 1, 1, 1, x, y);
+			std::cout << "x: " << x << " y: " << y << std::endl;
+			tp.calcDestinationCoords(0, 0, PI, 1, 1, 1, x, y);
+			std::cout << "x: " << x << " y: " << y << std::endl;
+			tp.calcDestinationCoords(0, 0, 0, 0, 1, 1, x, y);
+			std::cout << "x: " << x << " y: " << y << std::endl;
+			tp.calcDestinationCoords(0, 0, PI, 0, 1, 1, x, y);
+			std::cout << "x: " << x << " y: " << y << std::endl;
+			tp.calcDestinationCoords(0, 0, -PI/4, 0, 1, 1, x, y);
+			std::cout << "x: " << x << " y: " << y << std::endl;
+			*/
 		}
 
 		// Generate the velocity, acceleration profile and write it to a file
@@ -82,9 +135,9 @@ class TrajectoryPlanner
       }
 
 			// Initialize robot coordinates and yaw
-      float robot_x = path.poses[0].pose.position.x;
-      float robot_y = path.poses[0].pose.position.y;
-      float robot_yaw = findAngle(path.poses[0].pose, path.poses[1].pose);
+      double robot_x = path.poses[0].pose.position.x;
+      double robot_y = path.poses[0].pose.position.y;
+      double robot_yaw = findAngle(path.poses[0].pose, path.poses[1].pose);
       //Find nearest point to robot
       int index = find_index_nearest(path, robot_x, robot_y);
 
@@ -102,37 +155,21 @@ class TrajectoryPlanner
       }
 
       //Convert the look ahead point to the robot's local coordinates
-      float look_ahead_local_x, look_ahead_local_y;
+      double look_ahead_local_x, look_ahead_local_y;
       convertToLocalCoords(robot_x, robot_y, robot_yaw, look_ahead_pose.position.x, look_ahead_pose.position.y, look_ahead_local_x, look_ahead_local_y);
 
-			float current_vel = 0.0;
-			float acceleration = 0.1;
-			float PERIOD = 0.05; // 50ms
-			float acc_per_50_ms = acceleration * PERIOD;
+			double current_vel = 0.0;
+			double acceleration = 0.1;
+			double PERIOD = 0.05; // 50ms
+			double acc_per_50_ms = acceleration * PERIOD;
 			// Distance traveled in 50ms using current velocity
-			float distance_traveled = current_vel + 0.5 * pow(acc_per_50_ms, 2);
-			// Use distance = radius * theta to find theta
-			float radius = pow(distance(look_ahead_local_x, look_ahead_local_y, robot_x, robot_y), 2) / (2 * (look_ahead_local_x - robot_x) )
-			float theta = distance_traveled / radius;
-			// Rotate current x and y by theta around the origin of the circle to find the new local x and local y
-			float temp_x;
-			if(look_ahead_local_x > 0)
-			{
-				temp_x = -radius;
-			}
-			else
-			{
-				temp_x = radius
-			}
-			float temp_x = -radius;
-			float temp_y = 0;
-			rotate(theta, temp_x, temp_y);
-			// Convert new local x and local y to global x and global y
-			float new_robot_x = robot_x + temp_x;
-			float new_robot_y = robot_y + temp_y;
+			double distance_traveled = current_vel + 0.5 * pow(acc_per_50_ms, 2);
+			// Find the next point that the robot travelled to using distance_traveled
+			double new_robot_x, new_robot_y;
+			calcDestinationCoords(robot_x, robot_y, robot_yaw, distance_traveled, look_ahead_local_x, look_ahead_local_y, new_robot_x, new_robot_y);
 
 			// Find velocity for next point
-			float new_vel;
+			double new_vel;
 			if(new_vel < 0.3)
 			{
 				new_vel = current_vel + acc_per_50_ms;
@@ -144,8 +181,8 @@ class TrajectoryPlanner
 
 			// Find acceleration between current point and next point
 			// Find change in yaw between current point and next point
-			float curvature = 1.0 / radius;
-			float delta_yaw = robot_yaw + curvature * distance_traveled;
+			// double curvature = 1.0 / radius;
+			// double delta_yaw = robot_yaw + curvature * distance_traveled;
 		}
 
 		void writeTrajectoryToFile()
